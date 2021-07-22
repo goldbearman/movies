28a00664df01f0e99d3d727ead9947dbd8f61708
fsabirov <sabirovfarit@gmail.com>
2021-07-22 13:22:56 +0300

    genres / done

--- src/components/movie/genres.js
+++ src/components/movie/genres.js
@@ -1,30 +1,29 @@
 import React from 'react'
 
 
-
-
-const Genres =({allGenres,movieGenres})=>{
-    const elements = movieGenres.map((item) => {
-
-        allGenres.map((genre)=>{
-
-        })
-
-
-        const { id } = item;
+const Genres = ({allGenres, movieGenres}) => {
+    console.log(allGenres);
+    console.log(movieGenres);
+    const elements = movieGenres.map((numMovieGenres) => {
+        console.log(numMovieGenres);
+        // if(item.)
+        let name =  allGenres.reduce((sum, objGenres) => {
+            // console.log(objGenres);
+            if (objGenres.id === numMovieGenres) {
+                console.log(objGenres.name)
+                return sum + objGenres.name;
+            }
+            return sum;
+        },'')
+        console.log(name);
 
         return (
-            <button> </button>
-            // <Task
-            //     key={id}
-            //     {...item}
-            //     onDeleleted={() => onDeleted(id)}
-            //     onToggleDone={() => onToggleDone(id)}
-            // />
+            <button disabled>{name}</button>
         );
     });
+    console.log(elements);
 
-    return <ul className="todo-list">{elements}</ul>;
+    return <div className="genre">{elements}</div>;
 
 }
 
--- src/components/movie/movie.js
+++ src/components/movie/movie.js
@@ -6,7 +6,6 @@ import {format} from 'date-fns'
 import Genres from "./genres";
 
 
-
 import 'antd/dist/antd.css';
 import "./movie.css"
 
@@ -15,100 +14,99 @@ import {Row, Col, Slider} from 'antd';
 
 export default class Movie extends PureComponent {
 
-  state = {
-    movie: {},
-    rateDefault: 0,
-    allGenres: [],
-    rating:0
-  };
+    state = {
+        movie: {},
+        rateDefault: 0,
+        allGenres: [],
+        rating: 0
+    };
 
-  componentDidMount() {
-    this.setMovie();
-    this.getGenres();
-  }
-
-  setMovie() {
-    this.setState({
-      movie: this.props.movie,
-      rateDefault: this.props.movie.rating
-    })
-  }
-
-  swapiService = new SwapiService();
-
-
-  getGenres = () => {
-    this.props.getAllGenres().then(arrgenres => {
-      console.log(arrgenres.genres);
-      this.setState({
-        allGenres: arrgenres.genres
-      });
-    });
-  }
-
-
-  trimText(text) {
-    // const node = this.myRef.current;
-    // console.log(node)
-    if (text.length > 15) {
-      let new_str = text.slice(0, 130) + '...'
-      return new_str
+    componentDidMount() {
+        this.setMovie();
+        this.getGenres();
     }
-    return text;
-  }
 
-  onColorRate(rate) {
-    let setColor = "rating";
-    if (rate > 10 && rate< 0) setColor += " brown";
-    if (rate<= 3 && rate >= 0) setColor += " red";
-    if (rate<= 5 && rate > 3) setColor += " orange";
-    if (rate <= 7 && rate > 5) setColor += " yellow";
-    if (rate <= 10 && rate > 7) setColor += " green";
-    return setColor;
-  }
+    setMovie() {
+        this.setState({
+            movie: this.props.movie,
+            rateDefault: this.props.movie.rating
+        })
+    }
 
-  onChangeStars = (stars) => {
-    console.log(stars);
-
-    this.setState({
-      rateDefault: stars
-    });
-    console.log(this.state.rateDefault);
-    this.swapiService.postRate(stars, this.props.movie.id, this.props.guestSessionId).then(result => console.log(result))
-  }
+    swapiService = new SwapiService();
 
 
-  render() {
+    getGenres = () => {
+        this.props.getAllGenres().then(arrgenres => {
+            console.log(arrgenres.genres);
+            this.setState({
+                allGenres: arrgenres.genres
+            });
+        });
+    }
 
-    const {movie: {vote_average, original_title, overview, poster_path, release_date, rating, genre_ids}} = this.props
 
-    console.log(release_date)
-    let date = format(new Date(release_date), 'MMMM dd, yyyy');
-    console.log(this.props.movie.id)
-    console.log(this.state.rateDefault +" rate")
+    trimText(text) {
+        if (text.length > 15) {
+            let new_str = text.slice(0, 100) + '...'
+            return new_str
+        }
+        return text;
+    }
 
-    return (
-      <Col span={12}>
-        <div className='card'>
-          <img className="card__poster" src={`https://image.tmdb.org/t/p/w185/${poster_path}`}
-               alt="Poster"/>
-          <div className="card__content">
-            <div className={this.onColorRate(vote_average,)}>
-              <div>{vote_average}</div>
-            </div>
-            <h1>{original_title}</h1>
-            <div className="date">{date}</div>
-            <div className='genre'>
-              <button>War</button>
-            </div>
-            <Genres allGenres={this.state.allGenres} movieGenres={genre_ids}/>
-            <p>
-              {this.trimText(overview)}
-            </p>
-            <Rate className="stars" count="10" defaultValue={this.state.rateDefault} onChange={this.onChangeStars} value={this.state.rateDefault}/>
-          </div>
-        </div>
-      </Col>
-    )
-  }
+    onColorRate(rate) {
+        let setColor = "rating";
+        if (rate > 10 && rate < 0) setColor += " brown";
+        if (rate <= 3 && rate >= 0) setColor += " red";
+        if (rate <= 5 && rate > 3) setColor += " orange";
+        if (rate <= 7 && rate > 5) setColor += " yellow";
+        if (rate <= 10 && rate > 7) setColor += " green";
+        return setColor;
+    }
+
+    onChangeStars = (stars) => {
+        this.setState({
+            rateDefault: stars
+        });
+        this.swapiService.postRate(stars, this.props.movie.id, this.props.guestSessionId).then(result => console.log(result))
+    }
+
+    render() {
+
+        const {
+            movie: {
+                vote_average,
+                original_title,
+                overview,
+                poster_path,
+                release_date,
+                rating,
+                genre_ids
+            }
+        } = this.props
+
+        let date = format(new Date(release_date), 'MMMM dd, yyyy');
+
+        return (
+            <Col span={12}>
+                <div className='card'>
+                    <img className="card__poster" src={`https://image.tmdb.org/t/p/w185/${poster_path}`}
+                         alt="Poster"/>
+                    <div className="card__content">
+                        <div className={this.onColorRate(vote_average,)}>
+                            <div>{vote_average}</div>
+                        </div>
+                        <h1>{original_title}</h1>
+                        <div className="date">{date}</div>
+                        <Genres allGenres={this.state.allGenres} movieGenres={genre_ids}/>
+                        <p>
+                            {this.trimText(overview)}
+                        </p>
+                        <Rate className="stars" count="10" defaultValue={this.state.rateDefault}
+                              onChange={this.onChangeStars} value={this.state.rateDefault}/>
+                    </div>
+                </div>
+            </Col>
+        )
+    }
 }
